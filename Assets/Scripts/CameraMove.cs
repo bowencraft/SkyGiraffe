4fef1322c0fd4989ed19828ff37ed27eb5820006
Bowen <1758364810@qq.com>
2023-02-12 22:32:39 -0500

    Added sprites sheet & updated main scene

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -7,6 +7,7 @@ public class CameraMove : MonoBehaviour
     // Start is called before the first frame update
     public GameObject PlayerObject;
     public float CameraSpeed = 0.005f;
+    public int CameraOffset = 3;
     void Start()
     {
 
@@ -18,7 +19,7 @@ public class CameraMove : MonoBehaviour
         Vector3 newPos = transform.position;
 
         newPos.x = Mathf.Lerp(newPos.x, PlayerObject.transform.position.x, CameraSpeed);
-        newPos.y = Mathf.Lerp(newPos.y, PlayerObject.transform.position.y, CameraSpeed);
+        newPos.y = Mathf.Lerp(newPos.y, PlayerObject.transform.position.y + CameraOffset, CameraSpeed);
 
         transform.position = newPos;
 
